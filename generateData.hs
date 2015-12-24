3f718b0db38c1fce3720c82ea1d2a7f9914361c0
Valery Bugakov <skymk1@gmail.com>
2015-12-24 14:55:17 +0400

    tweaked text files generation script

--- generateData.hs
+++ generateData.hs
@@ -1,11 +1,19 @@
 import System.Random
 import System.IO.Unsafe
 import System.IO
+import System.Environment
+
+main :: IO()
+main = do
+    -- writeFile "./data/gen1.txt" (unlines $ map (\a -> (randomStr 10000)) [1..10000])
+    [fname, linesNumber, lineLength] <- getArgs
+    f <- openFile fname WriteMode
+    writeLines f (read linesNumber :: Int) (read lineLength :: Int)
+    hClose f
+
+writeLines :: Handle -> Int -> Int -> IO ()
+writeLines _ 0 _ = print "done"
+writeLines f n l = hPutStrLn f (randomStr l) >> writeLines f (n - 1) l
 
 randomStr :: Int -> String
 randomStr n = take n $ randomRs ('a','z') $ unsafePerformIO newStdGen
-
-main = do
-    writeFile "./data/gen1.txt" (unlines $ map (\a -> (randomStr 10000)) [1..10000])
-    print "done"
-
